df2ae3b56263d09631dfcba0ae973a1732dddb34
froec <froechrissi@googlemail.com>
2024-10-23 13:47:44 +0200

    Delete create_plots.bat

--- create_plots.bat
+++
@@ -1,5 +0,0 @@
-call conda activate base && ^
-call python create_plots.py --plotdir results_acs/ && ^
-call python create_plots.py --plotdir results_framingham/ && ^
-call python create_plots.py --plotdir results_celeba/ && ^
-call python create_entropy_comparison_plot.py
